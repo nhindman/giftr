d18f26d7d6b5195f49c0ddd27f5fc44e8a27b17c
nhindman <nathaniel.hindman@gmail.com>
2014-03-07 01:25:26 -0500

    fixing select friends button

--- config/application.rb
+++ config/application.rb
@@ -1,32 +1,70 @@
-require File.expand_path('../boot', __FILE__)
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Giftr</title>
+  <link href='http://fonts.googleapis.com/css?family=Gafata|Prosto+One|Alegreya+SC|Patua+One|Raleway|PT+Serif' rel='stylesheet' type='text/css'>
+  <%= stylesheet_link_tag    "application", :media => "all" %>
+  <%= javascript_include_tag "application" %>
+  <%= csrf_meta_tags %>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href='http://fonts.googleapis.com/css?family=Poiret+One|Ubuntu|Josefin+Slab|Love+Ya+Like+A+Sister|Megrim|Roboto+Slab|Allan|Bubbler+One|Roboto' rel='stylesheet' type='text/css'>
+  <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
 
-# Pick the frameworks you want:
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+  <!-- Script to make fullPage sticky scrolling work. using media query that disable sticky scrolling if page is less than 500px -->
+  <script type="text/javascript">
+    $(document).ready(function() {
+    if (mq.matches) {
+       $.fn.fullpage({   
+          resize : false, // Stops the splash page from resizing it's text when the page width is changed.
+        anchors: ['firstPage', 'secondPage', '3rdPage', 'lastPage'] //Tells fullPage that we're scrolling through four page sections.
+        });
+  // window width is at least 500px
+  console.log("at least 500px")
+} else {
+  console.log ("else works")
+  $.fn.fullpage({
+   autoScrolling: false,            
+   resize : false, // Stops the splash page from resizing it's text when the page width is changed.
+          
+   anchors: ['firstPage', 'secondPage', '3rdPage', 'lastPage'] //Tells fullPage that we're scrolling through four page sections.
+        })};
+    }); 
+  </script>
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
-Bundler.require(:default, Rails.env)
+</head>
+<body>
 
-module Giftr
-  class Application < Rails::Application
+<nav id="navigation" class="navbar navbar-inverse navbar-fixed-top" role="navigation">
 
-    config.assets.initialize_on_precompile=false
-    config.logger = Logger.new(STDOUT)
-    # config.serve_static_assets=true
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+  <div class="container">
 
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    <a class="navbar-brand" href=<%= root_path %>>Giftr</a>
 
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
-  end
-end
+    <button class="navbar-toggle" data-toggle="collapse" data-target=".navHeaderCollapse">
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+
+    <div class="collapse navbar-collapse navHeaderCollapse">
+      <ul class="signin nav navbar-nav navbar-right">
+        <% if user_signed_in? %>
+          <li><%= link_to "New Poll", polls_path %></li>
+          <li class="active"><%= link_to "Sign Out", destroy_user_session_path %></li>
+        <% else %>
+          <li><%= link_to "Sign In with Facebook", user_omniauth_authorize_path(:facebook) %></li>
+        <% end %>
+      </ul>
+    </div><!-- END .navbar-collapse -->
+    
+  </div><!-- END .container-fluid -->
+</nav>
+
+<!-- <div class="container-fluid"> -->
+ 
+    <%= yield %>
+
+<!-- </div> -->
+
+</body>
+</html>
