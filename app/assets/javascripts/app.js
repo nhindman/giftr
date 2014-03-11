701e008a34534dc5ad1c3785cd97467c50e0bcc2
nhindman <nathaniel.hindman@gmail.com>
2014-03-10 20:10:48 -0400

    testing FB Feed api

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -26,11 +26,11 @@ function resetSelector(){
             scrape: true
           }, function(response){
             FB.ui({
-            method: 'apprequests',
-            message: 'Hello People',
-            title: 'A friendly hello',
-            data: 'some-data',
-            access_token: 'https://graph.facebook.com/endpoint?key=value&access_token=561575507272927|9a56942c3ef85fa93fa4b116118c6d86'  
+            method: 'feed',
+            to: [accomplice],
+            link: "http://shielded-sands-2955.herokuapp.com/polls/" + poll_id,
+            caption: 'hi',
+            // access_token: 'https://graph.facebook.com/endpoint?key=value&access_token=561575507272927|9a56942c3ef85fa93fa4b116118c6d86'  
             }, fbCallback)
           })
         }
